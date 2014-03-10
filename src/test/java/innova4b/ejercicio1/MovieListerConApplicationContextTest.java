d98beb7218a6c3cb1cc5b35ac7274ed1de4eaf56
Maite <maite.yanguas@gmail.com>
2014-03-10 20:05:17 +0100

    Mejora en uso de ApplicationContext

--- src/test/java/innova4b/ejercicio1/MovieListerConApplicationContextTest.java
+++ src/test/java/innova4b/ejercicio1/MovieListerConApplicationContextTest.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,19 +18,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("classpath:innova4b/ejercicio1/application-context.xml")
 public class MovieListerConApplicationContextTest {
 	
-	GenericApplicationContext context;
+	ApplicationContext context;
 	
 	@Before
 	public void setUp() {
-		context = new GenericApplicationContext();  
-	    XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(context);  
-	    xmlReader.loadBeanDefinitions(new ClassPathResource("innova4b/ejercicio1/application-context.xml"));  
-	    context.refresh();  
+		context = new ClassPathXmlApplicationContext("innova4b/ejercicio1/application-context.xml");  
 	}
 
 	@Test
 	public void encuentra_las_peliculas_en_un_archivo() {
 		MovieLister movieLister = context.getBean("movieListerFromText", MovieLister.class);
+		
 		Movie[] movies = movieLister.moviesDirectedBy("Almodovar");
 		assertEquals(2, movies.length);
 	}
